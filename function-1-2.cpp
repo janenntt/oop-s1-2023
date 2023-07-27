45cc5cb139da4e2b957f88dba9d374c32682acd9
Tram Nguyen <tramnguyenntt@Janes-MacBook.local>
2023-07-28 01:39:27 +0930

    Adding changes(1) to 1-2

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -2,15 +2,16 @@
 
 double array_mean(int array[], int n){
     if(n<1){
-        return 0;
+        return 0.0;
     }
 
     int sum=0;
-    double average=0;
     for(int i=0;i<n;i++){
         sum=sum+array[i];
-        average = sum/n; 
-
     }
+
+    double average=static_cast<double>(sum)/n;
+   
     return average;
+
 }
