59a7d6c41005e859e5b8ee3a7d393863ced4d32a
Tram Nguyen <tramnguyenntt@student-10-201-02-195.wireless.adelaide.edu.au>
2023-07-28 17:47:08 +0930

    Adding changes(2) to 3-2

--- main-3-2.cpp
+++ main-3-2.cpp
@@ -5,8 +5,8 @@ extern int median_array(int array[], int n);
 
 int main(){
     int n; 
-    int array[5] = {1,5,3,4,2};
-    int result = median_array(array,5);
+    int array[7] = {1,5,3,4,2,9,6};
+    int result = median_array(array,7);
 
     cout << "The median of the array is: " << result << endl;
 
