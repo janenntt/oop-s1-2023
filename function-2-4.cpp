5928ee1a7ce113e53d95095754643640597ccad0
Tram Nguyen <tramnguyenntt@student-10-201-02-195.wireless.adelaide.edu.au>
2023-07-28 11:37:43 +0930

    Adding changes(1) to 2-4

--- function-2-4.cpp
+++ function-2-4.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 
 bool is_ascending(int array[], int n){
+
     if(n<=0){
         return false;
     }
 
-    for(int i=0; i<n; i++){
-        if(array[i] < array[i-1]){
+    for(int i=0;i<n;i++){
+        if(array[i]<array[i-1]){
             return false;
         }
     }
-    return true;
 
+    return true;
 }
--- main-2-4.cpp
+++ main-2-4.cpp
@@ -4,11 +4,12 @@ using namespace std;
 extern bool is_ascending(int array[], int n);
 
 int main(){
+
     int array[5] = {1,2,3,4,5};
-
     int result = is_ascending(array,5);
+    cout << "Are the elements in the array in ascending order? " << result << endl;
 
-    cout << "The elements in the array is in an ascending order? " << result << endl;
 
     return 0;
+    
 }
