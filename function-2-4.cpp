5eaaef905af2d955b0a37038ba5e8fd468ae6e2f
Tram Nguyen <tramnguyenntt@student-10-201-02-195.wireless.adelaide.edu.au>
2023-07-28 11:57:01 +0930

    Adding changes(2) to 2-4

--- function-2-4.cpp
+++ function-2-4.cpp
@@ -7,7 +7,7 @@ bool is_ascending(int array[], int n){
     }
 
     for(int i=0;i<n;i++){
-        if(array[i]<array[i-1]){
+        if(array[i-1]>array[i]){
             return false;
         }
     }
