feace9ca1164f37a118ec57c878973fff563a566
Tram Nguyen <tramnguyenntt@Janes-MacBook.local>
2023-08-01 02:08:05 +0930

    adding 1-5

---
+++ function-1-5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void print_summed(int array1[3][3], int array2[3][3]){
+    int result[3][3];
+
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            result[i][j] = array1[i][j]+array2[i][j];
+        }
+    }
+
+     for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            printf("%d ", result[i][j]);
+        }
+
+        printf("\n");
+
+     }
+
+}
