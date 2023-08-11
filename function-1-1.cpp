ae2c5508f6878562c0de4811d99c7e3ee58e6d26
Tram Nguyen <tramnguyenntt@student-10-201-03-121.wireless.adelaide.edu.au>
2023-08-11 16:53:42 +0930

    adding 1-1(1)

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void print_matrix(int array[10][10]){
     for(int i=0;i<10;i++){
         for(int j=0;j<10;j++){
-            printf(" %d", array[i][j]);
+            printf("%d ", array[i][j]);
         }
         printf("\n");
     }
