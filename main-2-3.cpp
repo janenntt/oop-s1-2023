45b7755f097ade414f3bc22de9faad7ef3c6a8eb
Tram Nguyen <tramnguyenntt@student-10-201-03-121.wireless.adelaide.edu.au>
2023-08-11 16:45:58 +0930

    adding 2-3(1)

--- main-2-3.cpp
+++ main-2-3.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
-extern int sum_if_palindrome(int integers[],int length);
-extern bool is_palindrome(int integers[], int length);
-extern int sum_array_elements(int integers[], int length);
+extern int palindrome_sum(int integers[],int length);
+extern bool is_array_palindrome(int integers[], int length);
+extern int sum_integers(int integers[], int length);
 
 int main(){
     int integers[6] = {1,2,3,3,2,1};
     
-    int result = sum_if_palindrome(integers, 6);
+    int result = palindrome_sum(integers, 6);
 
     if(result==-2){
         cout << "The array is not a palindrome array" << endl;
     } else {
-        cout << "Sum of elements = " << sum_if_palindrome(integers,6) << endl;
+        cout << "Sum of elements = " << palindrome_sum(integers,6) << endl;
     }
    
     return 0;
