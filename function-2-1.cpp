e1cbd812c8008c35f9ee744f54e328987bd669a9
Tram Nguyen <tramnguyenntt@student-10-201-03-001.wireless.adelaide.edu.au>
2023-08-15 17:29:55 +0930

    adding 2-1

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
+int* readNumbers(){
+    int *arr;
+    arr = new int[10];
+
+    for(int i=0; i<10; i++){
+        cin>>*(arr+i);
+    }   
+
+    return arr;
+
+}   
+
+void printNumbers(int *numbers, int length){
+    for(int i=0; i<length; i++){
+    cout<< i << " " << *(numbers+i) << endl;
+    }
+}
+
 void hexDigits(int *numbers, int length){
     for(int i=0;i<length;i++){
         cout << i << " " << numbers[i] << " ";
--- main-2-1.cpp
+++ main-2-1.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 extern void hexDigits(int *numbers, int length);
+extern int *readNumbers();
+extern void printNumbers(int *numbers, int length);
 
-int main()
-{
-    int numbers[]={11,12,3,4,15,6,7,8,9,10};
+int main(){
+    int* numbers;
+    numbers = readNumbers();
     hexDigits(numbers,10);
     return 0;
 }
