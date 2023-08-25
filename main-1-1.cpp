eefaa8c72ff40b30c2c22bd74e149b875102a23b
Tram Nguyen <tramnguyenntt@student-10-201-03-001.wireless.adelaide.edu.au>
2023-08-25 17:16:41 +0930

    adding (2)

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include "Asset.h"
-using namespace std;
 
-int main(){
-  
+int main() {
+    // Test default constructor
+    Asset defaultAsset;
+    std::cout << "Default Asset - Type: " << defaultAsset.get_product_type()
+              << ", Value: " << defaultAsset.get_value() << std::endl;
+
+    // Test parameterized constructor
+    Asset customAsset(10000, "Stock");
+    std::cout << "Custom Asset - Type: " << customAsset.get_product_type()
+              << ", Value: " << customAsset.get_value() << std::endl;
+
     return 0;
 }
