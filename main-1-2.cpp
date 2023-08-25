3b5e0ced8c6ffcd86e21ae9a672214aecc92d0c7
Tram Nguyen <tramnguyenntt@student-10-201-03-001.wireless.adelaide.edu.au>
2023-08-25 17:15:18 +0930

    adding AssetPortfolio

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -4,24 +4,6 @@
 using namespace std;
 
 int main() {
-    AssetPortfolio assetA(5, "notes");
-
-    Asset a1(10, "notes");
-    Asset a2(9, "notes");
-    Asset a3(11, "notes");
-
-    assetA.add_asset(a1);
-    assetA.add_asset(a2);
-    assetA.add_asset(a3);
-
-    cout << "Product type: " << assetA.get_name() << endl;
-    cout << "Current Number of assets: " << assetA.get_num_assets() << endl;
-
-    Asset* assets = assetA.get_assets();
-    cout << "Assets:" << endl;
-    for (int i = 0; i < assetA.get_num_assets(); ++i) {
-        cout << "Asset " << i + 1 << " - " << assets[i].get_product_type() << ", Value: " << assets[i].get_value() << endl;
-    }
-
+    
     return 0;
 }
