610c1aff1f76bb50838e1c152c54dbc972882ab0
Tram Nguyen <tramnguyenntt@student-10-201-03-001.wireless.adelaide.edu.au>
2023-08-25 17:00:20 +0930

    adding

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "Asset.h"
-#include <string>
 using namespace std;
 
 int main(){
+    Asset m1;
+    cout << "Default asset: " << m1.get_product_type() << ", Default value: " << m1.get_value() << endl;
+
+    Asset m2(5,"notes");
+    cout << "Asset: " << m2.get_product_type() << ", Value: " << m2.get_value() << endl;
 
     return 0;
 }
