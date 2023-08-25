5f9d1ea9116a06cc3807f34888676f3a3008302a
Tram Nguyen <tramnguyenntt@student-10-201-03-001.wireless.adelaide.edu.au>
2023-08-25 16:48:10 +0930

    adding Asset

---
+++ Asset.h
@@ -0,0 +1,22 @@
+#ifndef ASSET_H
+#define ASSET_H
+#include <string>
+using namespace std;
+
+class Asset{
+private:
+    string product_type;
+    int value;
+
+public:
+    Asset();
+    Asset(string product_type, int value);
+
+    string get_product_type();
+    int get_value();
+
+    ~Asset();
+
+};
+
+#endif
---
+++ main-1-1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Asset.h"
+#include <string>
+using namespace std;
+
+int main(){
+
+    return 0;
+}
