d1ce4032e039dd2047398e7eabd3767c2b0bf365
Tram Nguyen <tramnguyenntt@student-10-201-03-001.wireless.adelaide.edu.au>
2023-08-25 17:39:51 +0930

    adding 1

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 
 int main() {
+    AssetPortfolio portfolio(2);  
+
+    Asset asset1(5000, "Stock");
+    Asset asset2(10000, "Bond");
     
+
+    portfolio.add_asset(asset1);
+    portfolio.add_asset(asset2);
+
+
+    cout << "Number of assets in portfolio: " << portfolio.get_num_assets() << endl;
+  
+    cout << "Portfolio has Stock: " << (portfolio.has_asset("Stock") ? "Yes" : "No") <<endl;
+    cout << "Portfolio has Cryptocurrency: " << (portfolio.has_asset("Cryptocurrency") ? "Yes" : "No") << endl;
 
     return 0;
 }
