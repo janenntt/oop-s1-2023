c197a5652639aba6fcac997042e88c51601d1759
Tram Nguyen <tramnguyenntt@student-10-201-03-001.wireless.adelaide.edu.au>
2023-08-25 17:00:38 +0930

    adding 1

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
-#include "AssetPortfolio.h"
 #include "Asset.h"
-#include <string>
+#include "AssetPortfolio.h"
 using namespace std;
 
-int main(){
-    
+int main() {
+    AssetPortfolio assetA(5, "notes");
+
+    Asset a1(10, "notes");
+    Asset a2(9, "notes");
+    Asset a3(11, "notes");
+
+    assetA.add_assets(a1);
+    assetA.add_assets(a2);
+    assetA.add_assets(a3);
+
+    cout << "Product type: " << assetA.get_name() << endl;
+    cout << "Current Number of assets: " << assetA.get_num_assets() << endl;
+
+    Asset* assets = assetA.get_assets();
+    cout << "Assets:" << endl;
+    for (int i = 0; i < assetA.get_num_assets(); ++i) {
+        cout << "Asset " << i + 1 << " - " << assets[i].get_product_type() << ", Value: " << assets[i].get_value() << endl;
+    }
+
     return 0;
 }
