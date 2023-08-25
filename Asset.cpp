039b862898f02c05cc5cacf9096440105df3fd4c
Tram Nguyen <tramnguyenntt@student-10-201-03-001.wireless.adelaide.edu.au>
2023-08-25 17:58:48 +0930

    adding 1

--- Asset.cpp
+++ Asset.cpp
@@ -1,15 +1,16 @@
 #include <iostream> 
 #include <string> 
+#include "Asset.h"
 using namespace std; 
-class Asset{
-private:
-public:
-int value; 
-string product_type; 
-Asset();                            // a default constructor 
+Asset::Asset(): value(0), product_type(""){};                            // a default constructor 
 // a constructor that takes the product type  and value
-Asset(int value, std::string product_type);      
-std::string get_product_type();    // returns the financial asset type 
-int get_value();                   // returns the the value of asset
-~Asset();
+Asset::Asset(int value, std::string product_type): value(value), product_type(product_type){};      
+string Asset::get_product_type(){
+    return product_type;
+};    // returns the financial asset type 
+int Asset::get_value(){
+    return value; 
+};                   // returns the the value of asset
+Asset::~Asset(){
+
 };
--- Asset.h
+++ Asset.h
@@ -2,14 +2,14 @@
 #include <string> 
 using namespace std; 
 class Asset{
-private:
-public:
-int value; 
-string product_type; 
-Asset();                            // a default constructor 
-// a constructor that takes the product type  and value
-Asset(int value, std::string product_type);      
-std::string get_product_type();    // returns the financial asset type 
-int get_value();                   // returns the the value of asset
-~Asset();
+    private:
+    public:
+    int value; 
+    string product_type; 
+    Asset();                            // a default constructor 
+    // a constructor that takes the product type  and value
+    Asset(int value, std::string product_type);      
+    std::string get_product_type();    // returns the financial asset type 
+    int get_value();                   // returns the the value of asset
+    ~Asset();
 };
