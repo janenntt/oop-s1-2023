1215284adee023b7d58aaed6abc568e9bab808af
Tram Nguyen <tramnguyenntt@student-10-201-03-001.wireless.adelaide.edu.au>
2023-08-25 17:51:00 +0930

    adding 1

--- AssetPortfolio.cpp
+++ AssetPortfolio.cpp
@@ -41,32 +41,4 @@ bool AssetPortfolio::add_asset(Asset new_asset){
 AssetPortfolio::~AssetPortfolio(){
     delete [] a; 
 }  // destructor
- AssetPortfolio.h
- Download
-#include <iostream>
-#include "Asset.h"
-#include <string> 
-class AssetPortfolio{
-private:
-public:
-int count; 
-Asset *a; 
-int size; 
-AssetPortfolio();           // default constructor capacity of zero assets
-AssetPortfolio(int size);   // constructor for a portfolio of given size
-
-// returns the number of assets which are in the portfolio
-int get_num_assets(); 
-
-// returns true if any asset in the portfolio is of the specified product type
-// otherwise returns false
-bool has_asset(std::string product); 
-
-Asset *get_assets();       // returns the array of assets of the portfolio
-
-// returns true and adds new asset to the portfolio if the portfolio is not full
-// otherwise returns false
-bool add_asset(Asset new_asset);
-
-~AssetPortfolio();  // destructor
-};
+ 
