c2de0fc699da6431c16c3c4ee5fc30f62d512869
Tram Nguyen <tramnguyenntt@student-10-201-03-001.wireless.adelaide.edu.au>
2023-08-25 17:17:10 +0930

    adding AssetPortfolio

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-#include "Asset.h"
 #include "AssetPortfolio.h"
-using namespace std;
 
 int main() {
-    
+    AssetPortfolio portfolio(3);  // Create a portfolio with a capacity of 3 assets
+
+    // Add assets to the portfolio
+    Asset asset1(5000, "Stock");
+    Asset asset2(10000, "Bond");
+    Asset asset3(2000, "Real Estate");
+
+    portfolio.add_asset(asset1);
+    portfolio.add_asset(asset2);
+    portfolio.add_asset(asset3);
+
+    std::cout << "Number of assets in portfolio: " << portfolio.get_num_assets() << std::endl;
+
+    // Check if the portfolio has a certain asset type
+    std::cout << "Portfolio has Stock: " << (portfolio.has_asset("Stock") ? "Yes" : "No") << std::endl;
+    std::cout << "Portfolio has Cryptocurrency: " << (portfolio.has_asset("Cryptocurrency") ? "Yes" : "No") << std::endl;
+
     return 0;
 }
