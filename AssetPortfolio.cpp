dd37e844226533ea664ade35303666178fdd0257
Tram Nguyen <tramnguyenntt@student-10-201-03-001.wireless.adelaide.edu.au>
2023-08-25 17:48:42 +0930

    adding 1

--- AssetPortfolio.cpp
+++ AssetPortfolio.cpp
@@ -1,39 +1,72 @@
-#include "AssetPortfolio.h"
 #include <iostream>
-using namespace std;
-
-AssetPortfolio::AssetPortfolio() : capacity(0), num_assets(0), assets(nullptr) {}
-
-AssetPortfolio::AssetPortfolio(int size) : capacity(size), num_assets(0) {
-    assets = new Asset[size];
+#include "AssetPortfolio.h"
+using namespace std; 
+AssetPortfolio::AssetPortfolio(): count(0), size(0){
+    this-> a = new Asset[size]; 
+}           // default constructor capacity of zero assets
+AssetPortfolio::AssetPortfolio(int size):count(0), size(size){
+    this-> a = new Asset[size]; ;   // constructor for a portfolio of given size
 }
+// returns the number of assets which are in the portfolio
+int AssetPortfolio::get_num_assets(){
+    return count; 
+} 
 
-AssetPortfolio::~AssetPortfolio() {
-    delete[] assets;
-}
-
-int AssetPortfolio::get_num_assets() const {
-    return num_assets;
-}
-
-bool AssetPortfolio::has_asset(string product) const {
-    for (int i = 0; i < num_assets; i++) {
-        if (assets[i].get_product_type() == product) {
-            return true;
+// returns true if any asset in the portfolio is of the specified product type
+// otherwise returns false
+bool AssetPortfolio::has_asset(std::string product){
+    for (int i = 0; i < count; i++){
+        if (a[i].product_type == product){
+            return true; 
         }
     }
-    return false;
-}
+    return false; 
+} 
 
-Asset* AssetPortfolio::get_assets() const {
-    return assets;
-}
+Asset *AssetPortfolio::get_assets(){
+    return a; 
+}       // returns the array of assets of the portfolio
 
-bool AssetPortfolio::add_asset(Asset new_asset) {
-    if (num_assets < capacity) {
-        assets[num_assets] = new_asset;
-        num_assets++;
-        return true;
+// returns true and adds new asset to the portfolio if the portfolio is not full
+// otherwise returns false
+bool AssetPortfolio::add_asset(Asset new_asset){
+    if (count < size){
+        a[count] = new_asset; 
+        count++; 
+        return true; 
     }
-    return false;
+    return false; 
 }
+
+AssetPortfolio::~AssetPortfolio(){
+    delete [] a; 
+}  // destructor
+ AssetPortfolio.h
+ Download
+#include <iostream>
+#include "Asset.h"
+#include <string> 
+class AssetPortfolio{
+private:
+public:
+int count; 
+Asset *a; 
+int size; 
+AssetPortfolio();           // default constructor capacity of zero assets
+AssetPortfolio(int size);   // constructor for a portfolio of given size
+
+// returns the number of assets which are in the portfolio
+int get_num_assets(); 
+
+// returns true if any asset in the portfolio is of the specified product type
+// otherwise returns false
+bool has_asset(std::string product); 
+
+Asset *get_assets();       // returns the array of assets of the portfolio
+
+// returns true and adds new asset to the portfolio if the portfolio is not full
+// otherwise returns false
+bool add_asset(Asset new_asset);
+
+~AssetPortfolio();  // destructor
+};
--- AssetPortfolio.h
+++ AssetPortfolio.h
@@ -1,23 +1,27 @@
-#ifndef ASSETPORTFOLIO_H
-#define ASSETPORTFOLIO_H
+#include <iostream>
 #include "Asset.h"
-using namespace std;
-
-class AssetPortfolio {
-public:
-    AssetPortfolio();           
-    AssetPortfolio(int size);   
-    ~AssetPortfolio();          
-
-    int get_num_assets();
-    bool has_asset(string product);
-    Asset* get_assets();
-    bool add_asset(Asset new_asset);
-
+#include <string> 
+class AssetPortfolio{
 private:
-    int capacity;
-    int num_assets;
-    Asset* assets;
-};
+public:
+int count; 
+Asset *a; 
+int size; 
+AssetPortfolio();           // default constructor capacity of zero assets
+AssetPortfolio(int size);   // constructor for a portfolio of given size
 
-#endif 
+// returns the number of assets which are in the portfolio
+int get_num_assets(); 
+
+// returns true if any asset in the portfolio is of the specified product type
+// otherwise returns false
+bool has_asset(std::string product); 
+
+Asset *get_assets();       // returns the array of assets of the portfolio
+
+// returns true and adds new asset to the portfolio if the portfolio is not full
+// otherwise returns false
+bool add_asset(Asset new_asset);
+
+~AssetPortfolio();  // destructor
+};
