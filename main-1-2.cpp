54f7259448491fe5d1a6df54ae7a4cfa6e64e805
Tram Nguyen <tramnguyenntt@student-10-201-03-001.wireless.adelaide.edu.au>
2023-08-29 19:40:38 +0930

    adding

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -5,41 +5,7 @@
 #include "Motorbike.h"
 
 int main() {
-    ParkingLot parkingLot(10);
-
-    while (true) {
-        int choice;
-        std::cout << "Enter vehicle type (1 for Car, 2 for Bus, 3 for Motorbike): ";
-        std::cin >> choice;
-
-        if (choice < 1 || choice > 3) {
-            std::cout << "Invalid choice. Exiting." << std::endl;
-            break;
-        }
-
-        Vehicle* newVehicle;
-
-        if (choice == 1) {
-            newVehicle = new Car(parkingLot.getCount() + 1);
-        } else if (choice == 2) {
-            newVehicle = new Bus(parkingLot.getCount() + 1);
-        } else {
-            newVehicle = new Motorbike(parkingLot.getCount() + 1);
-        }
-
-        parkingLot.parkVehicle(newVehicle);
-
-        if (parkingLot.getCount() == parkingLot.getCount()) {
-            std::cout << "Parking lot is full." << std::endl;
-            break;
-        }
-    }
-
-    int unparkID;
-    std::cout << "Enter ID of vehicle to unpark: ";
-    std::cin >> unparkID;
-
-    parkingLot.unparkVehicle(unparkID);
+    
 
     return 0;
 }
