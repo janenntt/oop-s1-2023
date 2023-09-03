0c68abe4605835319bd15b8e6aaec09a9a8fe2ec
Tram Nguyen <tramnguyenntt@Janes-MacBook.local>
2023-09-04 01:05:29 +0930

    commit

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -7,12 +7,12 @@ using namespace std;
 int main(){
     Fridge samsung(500,100.0); 
 
-    cout << "Power rating of the fridge: " << samsung.get_powerRating() << " Watts" << endl;
+    cout << "Power rating of the fridge: " << samsung.set_powerRating() << " Watts" << endl;
     cout << "Volume of the fridge: " << samsung.getVolume() << " litres" << endl;
 
     samsung.turnOn();
 
-    cout << "Is the fridge turned on (1 - Yes, 0 - No) ? " << samsung.isTurnedOn() << endl;
+    cout << "Is the fridge turned on (1 - Yes, 0 - No) ? " << samsung.get_isOn() << endl;
     cout << "Power consumption of this fridge: " << samsung.getPowerConsumption() << "kWh" << endl;
 
     return 0;
