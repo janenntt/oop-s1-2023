499e8509928926b12283e659483f0915fabb1ac8
Tram Nguyen <tramnguyenntt@Janes-MacBook.local>
2023-09-04 01:52:02 +0930

    commit

--- main-2-2.cpp
+++ main-2-2.cpp
@@ -9,7 +9,7 @@ int main(){
     cout << "Power rating of the TV is: " << myTV.set_powerRating() << " Watts" << endl;
     cout << "Screen size of the TV is: " << myTV.getScreenSize() << " inches" << endl;
     cout << "Power consumption of the TV is: " << myTV.getPowerConsumption() << " kWh " << endl;
-    myTV.set_ScreenSize(55.0);
+    myTV.setScreenSize(55.0);
     cout << "Updated TV screen size: " << myTV.getScreenSize() << "inches" << endl;
     cout << "Updated power consumption of the TV is: " << myTV.getPowerConsumption() << " kWh " << endl;
     
