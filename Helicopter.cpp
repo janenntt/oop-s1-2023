53f4c7b064c552fe4441c9a8e1254348e47d3db4
Tram Nguyen <tramnguyenntt@student-10-201-03-032.wireless.adelaide.edu.au>
2023-09-08 18:10:48 +0930

    a

--- Helicopter.cpp
+++ Helicopter.cpp
@@ -1,31 +1,33 @@
-#include "Helicopter.h"  
-Helicopter::Helicopter(){
-    this->fuel = get_fuel(); 
-    this->numberOfFlights = 0;
-}; 
-Helicopter::Helicopter(int w, string n){
-    this->fuel = get_fuel(); 
-    this->numberOfFlights = 0;
-    this->weight = w; 
-    this->name = n; 
-}; 
+#include "AirVehicle.h"
+#include "Helicopter.h"
+#include <string>
+using namespace std;
+Helicopter::Helicopter(int w, string n): AirVehicle(w){
+    this->name = n;
+
+};
+
 string Helicopter::get_name(){
-    return this->name; 
-}; 
+    return this->name ;
+};
 void Helicopter::set_name(string n){
-    this->name = n; 
-}; 
-void Helicopter::fly(int headwind, int minutes){
-    if (headwind > 0){
-        if (headwind >= 40){
-          this->fuel -= 0.4*minutes; 
-        } 
-        else{
-        this->fuel -= 0.18*minutes; 
-        }
-    }
-    if (this->weight > 5670){
-        this->fuel  -= (this->weight-5670)*0.01*minutes; 
-    }
-    this->numberOfFlights += 1;
+    this->name=n;
+};
+ void Helicopter::fly(int headwind, int minutes){
+float fuel;
+if(headwind >=40 ){
+    fuel = 0.4;
+}else{
+    fuel = 0.18;
 }
+if(this->get_weight() -5670 >0 ){
+    fuel += 0.01*(this->get_weight()-5670);
+}
+
+float fuelUse = fuel*minutes;
+float fuelLeft = this->get_fuel() - fuelUse;
+if(fuelLeft > 20){
+this->numberOfFlights ++;
+this->set_fuel(fuelLeft);
+}
+ }
