2a9ea504d544a81377eb3bebc16a3af6bfd15315
Tram Nguyen <tramnguyenntt@student-10-201-03-032.wireless.adelaide.edu.au>
2023-09-08 17:34:15 +0930

    adding all

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 
 int main(){
+    AirVehicle a(3000);
 
+    a.set_numberOfFlights(0);
+    a.refuel();
+    a.fly(30,10);
 
+    cout << a.get_fuel() << endl;
+    cout << a.get_numberOfFlights() << endl;
     return 0;
 }
