842d5cb8b1f1e7f416cda682ab782308093b3c61
Tram Nguyen <tramnguyenntt@student-10-201-03-032.wireless.adelaide.edu.au>
2023-09-08 18:00:15 +0930

    a

--- AirFleet.cpp
+++ AirFleet.cpp
@@ -1,20 +1,18 @@
-#include "AirVehicle.h"
+#include "AirFleet.h"  
 #include "Helicopter.h"
 #include "Airplane.h"
-#include "AirFleet.h"
-
 AirFleet::AirFleet(){
-    Airplane a(20,10);
-    Helicopter b(10000, "BlackHawk");
-    AirVehicle c(5000);
-    Helicopter d(100, "WhiteHawk");
-    Airplane e(15,20);
-    fleet[0] = &a;
+    Airplane a(20,10); 
+    Helicopter b(10000, "BlackHawk"); 
+    AirVehicle c(5000); 
+    Helicopter d(100, "WhiteHawk");   
+    Airplane e(15, 20);     
+    fleet[0] = &a;  
     fleet[1] = &b;
     fleet[2] = &c;
     fleet[3] = &d;
     fleet[4] = &e;
-}
-AirVehicle ** AirFleet::get_fleet(){
-        return this->fleet; 
-}
+}; 
+AirVehicle** AirFleet::get_fleet(){
+    return this->fleet; 
+}; 
