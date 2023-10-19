496f7ba2d7c03235ef3264ad67c124b377bed2f0
Jane Nguyen <138750973+janenntt@users.noreply.github.com>
2023-10-19 05:16:27 +0000

    Update Utils.cpp

--- Utils.cpp
+++ Utils.cpp
@@ -8,8 +8,8 @@ std::tuple <int,int> Utils::generateRandomPos(int gridWidth, int gridHeight){
 
 double Utils::calculateDistance (std::tuple <int,int> pos1, std::tuple <int,int> pos2){
     int x1, y1, x2, y2;
-    pos1 = std::tie(x1, y1);
-    pos2 = std::tie(x2, y2);
+    std::tie(x1, y1) = pos1;
+    std::tie(x2, y2) = pos2;
     double dx = x1 - x2;
     double dy = y1 - y2;
     return std::sqrt(dx * dx + dy * dy);
