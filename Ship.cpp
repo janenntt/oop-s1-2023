8486b5851ab44e0f6ae70a4dc9149835ed23178d
Jane Nguyen <138750973+janenntt@users.noreply.github.com>
2023-10-19 05:15:11 +0000

    Update Ship.cpp

--- Ship.cpp
+++ Ship.cpp
@@ -1,10 +1,13 @@
 #include "Ship.h"
 
+Ship::Ship(int x, int y): GameEntity(x,y,'S'){}
+
 void Ship::move(int dx, int dy){
-
+    std::tuple <int,int> currentPos = getPos();
+    int current_x, current_y;
+    std::tie(current_x,current_y) = currentPos;
+    int new_x = current_x + dx;
+    int new_y = current_y + dy;
+    setPosition(std::make_tuple(new_x,new_y));
 }
 
-Ship::Ship(int x, int y, 'S'){
-    this -> x = x;
-    this -> y = y;
-}
