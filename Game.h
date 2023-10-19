603991dc143e295028a3f33a716ca95244e0f224
Jane Nguyen <138750973+janenntt@users.noreply.github.com>
2023-10-19 05:13:43 +0000

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,21 +1,20 @@
 #ifndef GAME_H
 #define GAME_H
+
 #include "GameEntity.h"
-#include "Effect.h"
-#include "Utils.h"
-#include "Explosion.h"
 #include "Ship.h"
 #include "Mine.h"
 #include <vector>
 
 class Game {
-    private:
-        std::vector<GameEntity*> entities;
-    public:
-        std::vector<GameEntity*> get_entities();
-        void set_entities(std::vector<GameEntity*> entities);
-        std::vector<GameEntity*> initGame(int numShips, int numMines, int gridWidth, int gridHeight);
-        void gameLoop(int maxIterations, double mineDistanceThreshold);
+private:
+    std::vector<GameEntity*> entities;
 
+public:
+    std::vector<GameEntity*> getEntities();
+    void setEntities(std::vector<GameEntity*> newEntities);
+    std::vector<GameEntity*> initGame(int numShips, int numMines, int gridWidth, int gridHeight);
+    void gameLoop(int maxIterations, double mineDistanceThreshold);
 };
+
 #endif
