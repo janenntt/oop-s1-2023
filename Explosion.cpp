7aa1895b50fc29ba13464d84c6c7f49f93a753e3
Jane Nguyen <138750973+janenntt@users.noreply.github.com>
2023-10-19 05:15:49 +0000

    Update Explosion.cpp

--- Explosion.cpp
+++ Explosion.cpp
@@ -1,10 +1,8 @@
 #include "Explosion.h"
 
-Explosion::Explosion(int x, int y, 'E'){
-    this -> x = x;
-    this -> y = y;
-}
+Explosion::Explosion(int x, int y) : GameEntity(x,y,'E'){}
 
 void Explosion::apply(GameEntity& entity){
-    
+    entity.setPosition(std::make_tuple(-1,-1));
+    entity.setType('X');
 }
