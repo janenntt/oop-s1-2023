1dd6ca41f3090a9519302b4780442debc189c30e
Jane Nguyen <138750973+janenntt@users.noreply.github.com>
2023-10-19 05:15:26 +0000

    Update main-2.cpp

--- main-2.cpp
+++ main-2.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
+#include "Ship.h"
+#include "Mine.h"
+#include "Explosion.h"
 
-int main(){
+int main() {
+    Ship myShip(10, 20);
+    Mine myMine(30, 40);
 
+    // Move the ship
+    myShip.move(5, 5);
+
+    // Explode the mine
+    Explosion explosion = myMine.explode();
+
+    // Apply the explosion to the ship
+    explosion.apply(myShip);
 
     return 0;
 }
