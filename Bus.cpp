6cd988d593697b14e3b53772c67050b571eca437
Tram Nguyen <138750973+janenntt@users.noreply.github.com>
2023-10-27 03:55:49 +0000

    Delete Bus.cpp

--- Bus.cpp
+++
@@ -1,9 +0,0 @@
-#include "Bus.h"
-
-Bus::Bus(int ID): Vehicle(ID){
-    timeOfEntry = std::time(nullptr);
-}
-
-int Bus::getParkingDuration() const {
-    return int(Vehicle::getParkingDuration()*0.75);
-}
